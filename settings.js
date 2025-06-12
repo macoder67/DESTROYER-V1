a07b183614983f4079fd3018b6ff6dde0b7acb8f
macoder67 <manyakaontiretse@gmail.com>
2025-06-12 16:05:13 +0200

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,14 @@
+const settings = {
+  packname: 'Destroyer v1',
+  session ID:''
+  author: 'Macoder‎',
+  botName: "DESTROYER-V1,
+  botOwner: 'Macoder', // Your name
+  ownerNumber: '27656136438', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "A SIMPLE JAVASCRIPT WHATSAPP BOT BY MACODER.",
+  version: "1.0.0 Beta",
+};
+
+module.exports = settings;
